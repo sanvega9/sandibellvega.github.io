f2c94369fc6df7a6c2fb2b3ed2a45172ebb701d2
Sandibell Vega <59941870+sanvega9@users.noreply.github.com>
2026-02-07 13:31:21 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import aiimage from "./image/ai.jpg";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col bg-cover bg-center bg-no-repeat">
@@ -18,7 +20,7 @@ export default function Home() {
    <div class="container mt-24 mx-auto px-5 py-4">
    <div className="absolute inset-0 bg-[#1C1678] opacity-45 z-[-1]"></div>
    <HeroSection/>
- 
+          <Footer/>
    </div>
     </main>
   );
