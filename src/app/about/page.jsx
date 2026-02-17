e82c0f9968c9b2a3d3a4fac14aba275c2d8cde97
Sandibell Vega <59941870+sanvega9@users.noreply.github.com>
2026-02-17 18:51:52 -0500

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -121,7 +121,7 @@ const About = () => {
     };
   }, []);
   useEffect(() => {
-  fetch('https://api.github.com/users/sanvega9/repos?sort=updated&per_page=6')
+  fetch('https://api.github.com/users/sanvega9/repos?sort=updated&per_page=9')
     .then(res => res.json())
     .then(data => setGithubRepos(data))
     .catch(err => console.error(err));
@@ -322,6 +322,7 @@ const About = () => {
 };
 
 export default About;
+
 
 
 
