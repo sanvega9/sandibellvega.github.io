a01deefcf3ae52f4dd29f7c58db4fe3bf6508b6b
Sandibell Vega <59941870+sanvega9@users.noreply.github.com>
2026-02-22 10:56:42 -0500

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,8 +28,8 @@ export const Navbar = () => {
   console.log(pathname);
     const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-        <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+  <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-black/40">        
+    <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
             <Link href={"/"} className='text-2xl md:text-5xl text-amber-600 font-semibold'>
             S.V
             </Link>
@@ -63,3 +63,4 @@ export const Navbar = () => {
   );
 };
 export default Navbar;
+
