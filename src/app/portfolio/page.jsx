e32f0a85a5d8f3a515fa1d055bafc265900ba09a
Sandibell Vega <59941870+sanvega9@users.noreply.github.com>
2026-02-24 22:10:15 -0500

    Update page.jsx

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -37,7 +37,7 @@ const Portfolio = () => {
   const ySlow = useTransform(scrollY, [0, 1000], [0, -200]);
   return (
     <div className="relative min-h-screen w-full overflow-x-hidden">
-     <div className="fixed inset-0 -z-10">
+     <div className="fixed inset-0 -z-20">
   {/* Base Image */}
   <motion.div
     style={{ y: ySlow }}
@@ -474,6 +474,7 @@ const ProjectCard = ({
 };
 
 export default Portfolio;
+
 
 
 
