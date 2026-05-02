24b7bf60cb9c3ba3bcd0e34a9d694b91cdcbc077
Sandibell Vega <59941870+sanvega9@users.noreply.github.com>
2026-05-01 20:26:06 -0400

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -92,7 +92,7 @@ const achievements = [
   }
 ];
 const experienceData = [
-  export const experience = [
+  export const experience = []
   {
     role: "STEM Instructor",
     company: "Concorde Education",
@@ -209,7 +209,6 @@ const experienceData = [
         color: "from-pink-500/10 to-transparent",
         linkedin_url: "https://www.linkedin.com/in/sandibell-vega-953889180/",
   },
-]
 ];
 const roles = [
   "All", "AI", "ML", "Robotics", "IoT", "UI/UX", "Frontend", "Backend", "Full-Stack",
